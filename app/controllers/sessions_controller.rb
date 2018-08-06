ee92c9c4d96df972495e2daa09615632aebad4a2
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-06 13:40:36 +0200

    view login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-   if User.all.include?(user)
-     log_in(user)
+    @user = User.find_by(email: params[:session][:email])
+   if User.all.include?(@user)
+     log_in(@user)
      redirect_to club_path
      puts 'connexion réussie'
    else
