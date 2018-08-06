eb1770050918fcb2582f724645d382f5a95411f5
Léona Chevrel <chevrel.leona@gmail.com>
2018-08-06 20:19:44 +0200

    tests on views ok

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,51 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+   @user = users(:one)
+  end
+
+  test 'Get to homepage' do
+    get '/'
+    assert_response :success
+  end
+
+  test 'Assert the presence of login links when logged out' do
+    get '/'
+    assert_select "a[href=?]", club_path, count: 0
+    assert_select "a[href=?]", login_path, count: 2
+  end
+
+  test 'Assert the presence of club link when logged in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    follow_redirect!
+    get '/'
+    assert_select "a[href=?]", club_path, count: 2
+    assert_select "a[href=?]", login_path, count: 0
+  end
+
+  test 'Assert redirection to login when logged_out' do
+    get '/club'
+    assert_redirected_to 'sessions#new'
+    follow_redirect!
+  end
+
+  test 'Assert possibility to access club page when logged_in' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    assert_redirected_to club_path
+    follow_redirect!
+  end
+
+  test 'Assert presence of a table containing all users on club page' do
+    get login_path
+    post login_path, params: { session: { email: @user.email  } }
+    assert_redirected_to club_path
+    follow_redirect!
+    #assert the presence of 3 rows in
+    assert_select 'table', count: 3
+  end
   # test "the truth" do
   #   assert true
   # end
