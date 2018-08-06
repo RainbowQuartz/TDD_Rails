7d63fca3b1ebed7059a49d3adee74b8705535f46
christopheleray <christophe.leray1@gmail.com>
2018-08-06 14:10:35 +0200

    staitc pages club ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :first_name, presence: true
-  validates :laste_name, presence: true
+  validates :last_name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                    format: { with: VALID_EMAIL_REGEX },
